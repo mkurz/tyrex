24e1897791e72541b101dd4674b8aae7016ede78
Mohammed Hussein <mohammed@users.sourceforge.net>
2001-10-05 22:15:13 +0000

    Superclass of internal XA resource classes in TransactionMaangerImpl and TransactionImpl

---
+++ src/main/tyrex/tm/impl/XAResourceHolder.java
@@ -0,0 +1,96 @@
+/**
+ * Redistribution and use of this software and associated documentation
+ * ("Software"), with or without modification, are permitted provided
+ * that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain copyright
+ *    statements and notices.  Redistributions must also contain a
+ *    copy of this document.
+ *
+ * 2. Redistributions in binary form must reproduce the
+ *    above copyright notice, this list of conditions and the
+ *    following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
+ *
+ * 3. The name "Exolab" must not be used to endorse or promote
+ *    products derived from this Software without prior written
+ *    permission of Intalio Technologies.  For written permission,
+ *    please contact info@exolab.org.
+ *
+ * 4. Products derived from this Software may not be called "Exolab"
+ *    nor may "Exolab" appear in their names without prior written
+ *    permission of Intalio Technologies. Exolab is a registered
+ *    trademark of Intalio Technologies.
+ *
+ * 5. Due credit should be given to the Exolab Project
+ *    (http://www.exolab.org/).
+ *
+ * THIS SOFTWARE IS PROVIDED BY EXOFFICE TECHNOLOGIES AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * EXOFFICE TECHNOLOGIES OR ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Copyright 2001 (C) Intalio Technologies Inc. All Rights Reserved.
+ *
+ */
+
+
+package tyrex.tm.impl;
+
+import javax.transaction.xa.XAResource;
+import tyrex.tm.XAResourceCallback;
+
+/////////////////////////////////////////////////////////////////////
+// XAResourceHolder
+/////////////////////////////////////////////////////////////////////
+
+/**
+ * Helper class containing XA resource and its associated 
+ * callback.
+ *
+ * @author <a href="mohammed@intalio.com">Riad Mohammed</a>
+ */
+class XAResourceHolder {
+
+    /**
+     * This class is used by both ThreadContext and 
+     * TransactionImpl. These classes contain private subclasses
+     * of this class that refer to the next holder. This class
+     * does not have a next field so as to avoid casting to the
+     * correct type in TransactionImpl.
+     */
+
+    /**
+     * The XA resource
+     */
+    final XAResource _xaResource;
+
+    /**
+     * The XA resource callback
+     */
+    final XAResourceCallback _callback;
+
+    /**
+     * Create the XAResourceHolder
+     *
+     * @param xaResource the XA resource (required)
+     * @param callback the XA resource callback (optional)
+     */
+    XAResourceHolder(XAResource xaResource,
+                     XAResourceCallback callback)
+    {
+        if (null == xaResource)
+            throw new IllegalArgumentException("Argument 'xaResource' is null");
+
+        _xaResource = xaResource;
+        _callback = callback;
+    }
+}
