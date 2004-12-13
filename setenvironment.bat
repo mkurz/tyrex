43bfa27e6eee26581817b746d4a24768eeb0a0ef
Rost Vashevnik <metaboss@users.sourceforge.net>
2004-12-13 23:20:18 +0000

    Changes JDK and ANT settings

--- setenvironment.bat
+++ setenvironment.bat
@@ -1,5 +1,5 @@
 @echo off
-REM $Id: setenvironment.bat,v 1.1 2004/04/30 06:36:50 metaboss Exp $
-SET JAVA_HOME=c:\jdk1.3.1_02
-SET ANT_HOME=c:\JavaLib\apache-ant-1.5.3-1
+REM $Id: setenvironment.bat,v 1.2 2004/12/13 23:20:18 metaboss Exp $
+SET JAVA_HOME=c:\j2sdk1.4.2_05
+SET ANT_HOME=c:\JavaLib\apache-ant-1.6.1
 SET PATH=%PATH%;%ANT_HOME%\bin
